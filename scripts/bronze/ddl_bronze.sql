a87dd02770f939df9277a79ba1c38503e8512cc0
daremau <daremauricio5@gmail.com>
2025-04-09 17:37:38 -0300

    Fix typo in column name from cst_create_data to cst_create_date in bronze.crm_cust_info table

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -7,7 +7,7 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_lastname NVARCHAR(50),
 	cst_marital_status NVARCHAR(50),
 	cst_gndr NVARCHAR(50),
-	cst_create_data DATE
+	cst_create_date DATE
 );
 
 IF OBJECT_ID('bronze.crm_prd_info') IS NOT NULL
