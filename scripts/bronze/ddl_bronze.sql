1c2884f26c0742733b6ed6defc94d7fc823b151f
daremau <daremauricio5@gmail.com>
2025-04-10 20:02:50 -0300

    Refactor column names to lowercase in bronze and silver layer tables

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -39,23 +39,23 @@ CREATE TABLE bronze.crm_sales_details (
 IF OBJECT_ID('bronze.erp_cust_az12') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12
 CREATE TABLE bronze.erp_cust_az12 (
-	CID NVARCHAR(50),
-	BDATE DATE,
-	GEN NVARCHAR(50)
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50)
 );
 
 IF OBJECT_ID('bronze.erp_loc_a101') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101
 CREATE TABLE bronze.erp_loc_a101 (
-	CID NVARCHAR(50),
-	CNTRY NVARCHAR(50)
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50)
 );
 
 IF OBJECT_ID('bronze.erp_px_cat_g1v2') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-	ID NVARCHAR(50),
-	CAT NVARCHAR(50),
-	SUBCAT NVARCHAR(50),
-	MAINTENANCE NVARCHAR(50)
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50)
 );
--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -43,26 +43,26 @@ CREATE TABLE silver.crm_sales_details (
 IF OBJECT_ID('silver.erp_cust_az12') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12
 CREATE TABLE silver.erp_cust_az12 (
-	CID NVARCHAR(50),
-	BDATE DATE,
-	GEN NVARCHAR(50),
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50),
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.erp_loc_a101') IS NOT NULL
 	DROP TABLE silver.erp_loc_a101
 CREATE TABLE silver.erp_loc_a101 (
-	CID NVARCHAR(50),
-	CNTRY NVARCHAR(50),
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50),
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.erp_px_cat_g1v2') IS NOT NULL
 	DROP TABLE silver.erp_px_cat_g1v2
 CREATE TABLE silver.erp_px_cat_g1v2 (
-	ID NVARCHAR(50),
-	CAT NVARCHAR(50),
-	SUBCAT NVARCHAR(50),
-	MAINTENANCE NVARCHAR(50),
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50),
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
